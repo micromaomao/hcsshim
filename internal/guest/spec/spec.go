micromaomao/hcsshim

commit 7084bd2fa445d83629a67a91ff4e072517a11f04

rego policy enforcer should use the same user parsing logic as GCS (#2405)
This PR fixes a discrepancy between user info handling between
GCS and rego policy enforcer. For example, GCS doesn't require the
user/group to exist in container's /etc/passwd and /etc/group
and has a fallback to UID and GID 0, when the user is absent.
Rego enforcer's `GetUserInfo`, however, always tries to
lookup user/group in /etc/passwd and /etc/group and returns
an error when the UID doesn't exist. This behavior is inconsistent
with non confidential LCOW workloads and fixed in this PR.

To avoid circular imports, the spec.go and spec_devices.go under
`internal/guest/runtime/hcsv2` have been moved under
`internal/guest/spec` and the dependent code updated accordingly.
As a result a bunch of methods are now exported, but still under
`internal`, so this shouldn't cause problems.

User parsing has been updated and split into `ParseUserStr`, which
returns UID and GID for a given `username` string and `SetUserStr`,
which just sets the UID and GID for the OCI process.

Rego enforcer's `GetUserInfo` now prioritizes the result of
`ParseUserStr` and fallbacks to the previous behavior of UID/GID
lookup in container's filesystem.

Signed-off-by: Maksim An <maksiman@microsoft.com>

pkg/securitypolicy/securitypolicyenforcer_rego.go
@@ -15,13 +15,14 @@ import (
 	"strings"
 	"syscall"
 
-	"github.com/Microsoft/hcsshim/internal/guest/spec"
-	"github.com/Microsoft/hcsshim/internal/guestpath"
-	"github.com/Microsoft/hcsshim/internal/log"
-	rpi "github.com/Microsoft/hcsshim/internal/regopolicyinterpreter"
 	"github.com/opencontainers/runc/libcontainer/user"
 	oci "github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/pkg/errors"
+
+	specGuest "github.com/Microsoft/hcsshim/internal/guest/spec"
+	"github.com/Microsoft/hcsshim/internal/guestpath"
+	"github.com/Microsoft/hcsshim/internal/log"
+	rpi "github.com/Microsoft/hcsshim/internal/regopolicyinterpreter"
 )
 
 const regoEnforcerName = "rego"
@@ -719,8 +720,8 @@ func (policy *regoEnforcer) EnforceCreateContainerPolicy(
 		"argList":              argList,
 		"envList":              envList,
 		"workingDir":           workingDir,
-		"sandboxDir":           spec.SandboxMountsDir(sandboxID),
-		"hugePagesDir":         spec.HugePagesMountsDir(sandboxID),
+		"sandboxDir":           specGuest.SandboxMountsDir(sandboxID),
+		"hugePagesDir":         specGuest.HugePagesMountsDir(sandboxID),
 		"mounts":               appendMountData([]interface{}{}, mounts),
 		"privileged":           privileged,
 		"noNewPrivileges":      noNewPrivileges,
@@ -1022,6 +1023,23 @@ func (policy *regoEnforcer) GetUserInfo(containerID string, process *oci.Process
 		return IDName{}, nil, "", errors.New("spec.Process is nil")
 	}
 
+	// this default value is used in the Linux kernel if no umask is specified
+	umask := "0022"
+	if process.User.Umask != nil {
+		umask = fmt.Sprintf("%04o", *process.User.Umask)
+	}
+
+	if process.User.Username != "" {
+		uid, gid, err := specGuest.ParseUserStr(rootPath, process.User.Username)
+		if err == nil {
+			userIDName := IDName{ID: strconv.FormatUint(uint64(uid), 10)}
+			groupIDName := IDName{ID: strconv.FormatUint(uint64(gid), 10)}
+			return userIDName, []IDName{groupIDName}, umask, nil
+		}
+		log.G(context.Background()).WithError(err).Warn("failed to parse user str, fallback to lookup")
+	}
+
+	// fallback UID/GID lookup
 	uid := process.User.UID
 	userIDName := IDName{ID: strconv.FormatUint(uint64(uid), 10), Name: ""}
 	if _, err := os.Stat(passwdPath); err == nil {
@@ -1069,12 +1087,6 @@ func (policy *regoEnforcer) GetUserInfo(containerID string, process *oci.Process
 			}
 			groupIDNames = append(groupIDNames, groupIDName)
 		}
-	}
-
-	// this default value is used in the Linux kernel if no umask is specified
-	umask := "0022"
-	if process.User.Umask != nil {
-		umask = fmt.Sprintf("%04o", *process.User.Umask)
 	}
 
 	return userIDName, groupIDNames, umask, nil

internal/guest/spec/spec.go
@@ -1,16 +1,30 @@
 //go:build linux
 // +build linux
 
-// TODO: consider moving oci spec specific code from /internal/guest/runtime/hcsv2/spec.go
-
 package spec
 
 import (
+	"context"
+	"fmt"
+	"math"
 	"path/filepath"
+	"strconv"
 	"strings"
 
-	"github.com/Microsoft/hcsshim/internal/guestpath"
+	"github.com/Microsoft/hcsshim/pkg/annotations"
+	"github.com/moby/sys/user"
+	"github.com/opencontainers/runc/libcontainer/devices"
 	oci "github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+
+	"github.com/Microsoft/hcsshim/internal/guestpath"
+	"github.com/Microsoft/hcsshim/internal/log"
+	"github.com/Microsoft/hcsshim/internal/logfields"
+)
+
+const (
+	devShmPath = "/dev/shm"
 )
 
 // networkingMountPaths returns an array of mount paths to enable networking
@@ -88,3 +102,310 @@ func HugePagesMountSource(sandboxID, path string) string {
 	subPath := strings.TrimPrefix(path, guestpath.HugePagesMountPrefix)
 	return filepath.Join(mountsDir, subPath)
 }
+
+// GetNetworkNamespaceID returns the `ToLower` of
+// `spec.Windows.Network.NetworkNamespace` or `""`.
+func GetNetworkNamespaceID(spec *oci.Spec) string {
+	if spec.Windows != nil &&
+		spec.Windows.Network != nil {
+		return strings.ToLower(spec.Windows.Network.NetworkNamespace)
+	}
+	return ""
+}
+
+// IsRootReadonly returns `true` if the spec specifies the rootfs is readonly.
+func IsRootReadonly(spec *oci.Spec) bool {
+	if spec.Root != nil {
+		return spec.Root.Readonly
+	}
+	return false
+}
+
+// removeMount removes mount from the array if `target` matches `Destination`
+func removeMount(target string, mounts []oci.Mount) []oci.Mount {
+	var result []oci.Mount
+	for _, m := range mounts {
+		if m.Destination == target {
+			continue
+		}
+		result = append(result, m)
+	}
+	return result
+}
+
+func setProcess(spec *oci.Spec) {
+	if spec.Process == nil {
+		spec.Process = &oci.Process{}
+	}
+}
+
+func SetCoreRLimit(spec *oci.Spec, value string) error {
+	setProcess(spec)
+
+	vals := strings.Split(value, ";")
+	if len(vals) != 2 {
+		return errors.New("wrong number of values supplied for rlimit core")
+	}
+
+	soft, err := strconv.ParseUint(vals[0], 10, 64)
+	if err != nil {
+		return errors.Wrap(err, "failed to parse soft core rlimit")
+	}
+	hard, err := strconv.ParseUint(vals[1], 10, 64)
+	if err != nil {
+		return errors.Wrap(err, "failed to parse hard core rlimit")
+	}
+
+	spec.Process.Rlimits = append(spec.Process.Rlimits, oci.POSIXRlimit{
+		Type: "RLIMIT_CORE",
+		Soft: soft,
+		Hard: hard,
+	})
+
+	return nil
+}
+
+// ParseUserStr parses `userStr`, looks up container filesystem's /etc/passwd and /etc/group
+// files for UID and GID for the process.
+//
+// NB: When `userStr` represents a UID, which doesn't exist, return UID as is with GID set to 0.
+func ParseUserStr(rootPath, userStr string) (uint32, uint32, error) {
+	parts := strings.Split(userStr, ":")
+	switch len(parts) {
+	case 1:
+		v, err := strconv.Atoi(parts[0])
+		if err != nil {
+			// this is a username string, evaluate uid/gid
+			u, err := getUser(rootPath, func(u user.User) bool {
+				return u.Name == userStr
+			})
+			if err != nil {
+				return 0, 0, errors.Wrapf(err, "failed to find user by username: %s", userStr)
+			}
+			return uint32(u.Uid), uint32(u.Gid), nil
+		}
+
+		// this is a UID, parse /etc/passwd to find GID.
+		u, err := getUser(rootPath, func(u user.User) bool {
+			return u.Uid == v
+		})
+		if err != nil {
+			// UID doesn't exist, return as is with GID 0.
+			if OutOfUint32Bounds(v) {
+				return 0, 0, errors.Errorf("UID (%d) exceeds uint32 bounds", v)
+			}
+			return uint32(v), 0, nil
+		}
+		return uint32(u.Uid), uint32(u.Gid), nil
+	case 2:
+		var (
+			userName, groupName string
+			uid, gid            uint32
+		)
+		v, err := strconv.Atoi(parts[0])
+		if err != nil {
+			// username string, lookup UID
+			userName = parts[0]
+			u, err := getUser(rootPath, func(u user.User) bool {
+				return u.Name == userName
+			})
+			if err != nil {
+				return 0, 0, errors.Wrapf(err, "failed to find user by username: %s", userName)
+			}
+			uid = uint32(u.Uid)
+		} else {
+			if OutOfUint32Bounds(v) {
+				return 0, 0, errors.Errorf("UID (%d) exceeds uint32 bounds", v)
+			}
+			uid = uint32(v)
+		}
+
+		v, err = strconv.Atoi(parts[1])
+		if err != nil {
+			// groupname string, lookup GID
+			groupName = parts[1]
+			g, err := getGroup(rootPath, func(g user.Group) bool {
+				return g.Name == groupName
+			})
+			if err != nil {
+				return 0, 0, errors.Wrapf(err, "failed to find group by groupname: %s", groupName)
+			}
+			gid = uint32(g.Gid)
+		} else {
+			if OutOfUint32Bounds(v) {
+				return 0, 0, errors.Errorf("GID (%d) exceeds uint32 bounds", v)
+			}
+			gid = uint32(v)
+		}
+		return uid, gid, nil
+	default:
+		return 0, 0, fmt.Errorf("invalid userstr: '%s'", userStr)
+	}
+}
+
+// SetUserStr sets `spec.Process` to the valid `userstr` based on the OCI Image Spec
+// v1.0.0 `userstr`.
+//
+// Valid values are: user, uid, user:group, uid:gid, uid:group, user:gid.
+// If uid is provided instead of the username then that value is not checked against the
+// /etc/passwd file to verify if the user with given uid actually exists.
+//
+// Since UID and GID are parsed as ints, but will ultimately end up as uint32 in the [OCI spec],
+// an error is returned if the the IDs are not within the uint32 bounds ([0, math.MathUint32]).
+// This avoid unexpected results if the ID is first parsed as an int and then
+// overflows around when downcast (eg, [math.MaxUint32] + 1 will become 0).
+// Notes:
+//   - Per the [Go spec], we have no indication of overflow when converting between integer types.
+//   - "man 5 passwd" and "man 5 group" (as well as [user.ParsePasswdFileFilter] and [user.ParseGroupFilter))
+//     do not specify any limits on the UID and GID range.
+//
+// [OCI spec]: https://pkg.go.dev/github.com/opencontainers/runtime-spec/specs-go#User
+// [Go spec]: https://go.dev/ref/spec#Conversions
+func SetUserStr(spec *oci.Spec, userstr string) error {
+	setProcess(spec)
+
+	uid, gid, err := ParseUserStr(spec.Root.Path, userstr)
+	if err != nil {
+		return err
+	}
+
+	spec.Process.User.UID, spec.Process.User.GID = uid, gid
+	return nil
+}
+
+// getUser looks up /etc/passwd file in a container filesystem and returns parsed user
+func getUser(rootPath string, filter func(user.User) bool) (user.User, error) {
+	users, err := user.ParsePasswdFileFilter(filepath.Join(rootPath, "/etc/passwd"), filter)
+	if err != nil {
+		return user.User{}, err
+	}
+	if len(users) != 1 {
+		return user.User{}, errors.Errorf("expected exactly 1 user matched '%d'", len(users))
+	}
+
+	if OutOfUint32Bounds(users[0].Uid) {
+		return user.User{}, errors.Errorf("UID (%d) exceeds uint32 bounds", users[0].Uid)
+	}
+
+	if OutOfUint32Bounds(users[0].Gid) {
+		return user.User{}, errors.Errorf("GID (%d) exceeds uint32 bounds", users[0].Gid)
+	}
+	return users[0], nil
+}
+
+// getGroup looks up /etc/group file in a container filesystem and returns parsed group
+func getGroup(rootPath string, filter func(user.Group) bool) (user.Group, error) {
+	groups, err := user.ParseGroupFileFilter(filepath.Join(rootPath, "/etc/group"), filter)
+	if err != nil {
+		return user.Group{}, err
+	}
+
+	if len(groups) != 1 {
+		return user.Group{}, errors.Errorf("expected exactly 1 group matched '%d'", len(groups))
+	}
+
+	if OutOfUint32Bounds(groups[0].Gid) {
+		return user.Group{}, errors.Errorf("GID (%d) exceeds uint32 bounds", groups[0].Gid)
+	}
+	return groups[0], nil
+}
+
+// ApplyAnnotationsToSpec modifies the spec based on additional information from annotations
+func ApplyAnnotationsToSpec(ctx context.Context, spec *oci.Spec) error {
+	// Check if we need to override container's /dev/shm
+	if val, ok := spec.Annotations[annotations.LCOWDevShmSizeInKb]; ok {
+		mt, err := devShmMountWithSize(val)
+		if err != nil {
+			return err
+		}
+		spec.Mounts = removeMount(devShmPath, spec.Mounts)
+		spec.Mounts = append(spec.Mounts, *mt)
+		log.G(ctx).WithField("sizeKB", val).Debug("set custom /dev/shm size")
+	}
+
+	var err error
+	privileged := false
+	if val, ok := spec.Annotations[annotations.LCOWPrivileged]; ok {
+		privileged, err = strconv.ParseBool(val)
+		if err != nil {
+			log.G(ctx).WithFields(logrus.Fields{
+				logfields.OCIAnnotation: annotations.LCOWPrivileged,
+				logfields.Value:         val,
+				logfields.ExpectedType:  logfields.Bool,
+			}).WithError(err).Warning("annotation value could not be parsed")
+		}
+	}
+	// Check if we need to do any capability/device mappings
+	if privileged {
+		log.G(ctx).Debugf("'%s' set for privileged container", annotations.LCOWPrivileged)
+
+		// Add all host devices
+		hostDevices, err := devices.HostDevices()
+		if err != nil {
+			return err
+		}
+		for _, hostDevice := range hostDevices {
+			AddLinuxDeviceToSpec(ctx, hostDevice, spec, false)
+		}
+
+		// Set the cgroup access
+		spec.Linux.Resources.Devices = []oci.LinuxDeviceCgroup{
+			{
+				Allow:  true,
+				Access: "rwm",
+			},
+		}
+	} else {
+		tempLinuxDevices := spec.Linux.Devices
+		spec.Linux.Devices = []oci.LinuxDevice{}
+		for _, ld := range tempLinuxDevices {
+			hostDevice, err := devices.DeviceFromPath(ld.Path, "rwm")
+			if err != nil {
+				return err
+			}
+			AddLinuxDeviceToSpec(ctx, hostDevice, spec, true)
+		}
+	}
+
+	return nil
+}
+
+// AddDevSev adds SEV device to container spec. On 5.x kernel the device is /dev/sev,
+// however this changed in 6.x where the device is /dev/sev-guest.
+func AddDevSev(ctx context.Context, spec *oci.Spec) error {
+	// try adding /dev/sev, which should be present for 5.x kernel
+	devSev, err := devices.DeviceFromPath("/dev/sev", "rwm")
+	if err != nil {
+		// try adding /dev/guest-sev, which should be present for 6.x kernel
+		var errSevGuest error
+		devSev, errSevGuest = devices.DeviceFromPath("/dev/sev-guest", "rwm")
+		if errSevGuest != nil {
+			return fmt.Errorf("failed to add SEV device to spec: %w: %w", err, errSevGuest)
+		}
+	}
+	AddLinuxDeviceToSpec(ctx, devSev, spec, true)
+	return nil
+}
+
+// devShmMountWithSize returns a /dev/shm device mount with size set to
+// `sizeString` if it represents a valid size in KB, returns error otherwise.
+func devShmMountWithSize(sizeString string) (*oci.Mount, error) {
+	size, err := strconv.ParseUint(sizeString, 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("/dev/shm size must be a valid integer: %w", err)
+	}
+	if size == 0 {
+		return nil, errors.New("/dev/shm size must be non-zero")
+	}
+
+	// Use the same options as in upstream https://github.com/containerd/containerd/blob/0def98e462706286e6eaeff4a90be22fda75e761/oci/mounts.go#L49
+	sizeKB := fmt.Sprintf("size=%sk", sizeString)
+	return &oci.Mount{
+		Source:      "shm",
+		Destination: devShmPath,
+		Type:        "tmpfs",
+		Options:     []string{"nosuid", "noexec", "nodev", "mode=1777", sizeKB},
+	}, nil
+}
+
+func OutOfUint32Bounds(v int) bool { return v < 0 || v > math.MaxUint32 }

commit ad23bcfc840a1c9b2c7eead20509c17ad1ca8691

guest/spec, rego: Fix user/group/additionalGIDs enforcement
A workload using a custom user for one of its container now fails to start with
policy denial: invalid user.

In the "broken" version, we see this in the input into Rego:

	...
	    "user": {
	      "id": "100",
	      "name": ""
	    },
	...

in the working version the input has

	...
	    "user": {
	      "id": "100",
	      "name": "curl_user"
	    },
	...

The issue seems to be that the new GetUserInfo, when given a valid userstr, does
not return any username/groupnames in the IDName structs, only IDs.  Therefore,
policies written to enforce the name will fail.

To fix this, we extend ParseUserStr to return the user and group names, and
update GetUserInfo to store the names in the IDName structs.  Since the function
now contains extra logic to e.g. fetch the group name even when we have known
UID/GID, some slight refactoring was done to avoid code repetition.

Looking at azcri container_create_windows.go (this file handles LCOW as well)
generateContainerSpec and container_create.go:generateUserString, my
understanding is that it would always set Username even if in the container JSON
it's given as run_as_user/run_as_group with a UID (but the username will be set
to a string of the integers).  The userstr that GCS sees would only be empty if
neither the container JSON nor the container image itself specify any users, in
which case we would default to uid 0 and gid 0.

Fixes: 7084bd2fa445 ("rego policy enforcer should use the same user parsing logic as GCS"...)
Signed-off-by: Tingmao Wang <tingmaowang@microsoft.com>

pkg/securitypolicy/securitypolicyenforcer_rego.go
@@ -1014,70 +1014,90 @@ func getGroup(groupPath string, filter func(user.Group) bool) (user.Group, error
 	return groups[0], nil
 }
 
-func (policy *regoEnforcer) GetUserInfo(containerID string, process *oci.Process) (IDName, []IDName, string, error) {
+func (policy *regoEnforcer) GetUserInfo(containerID string, process *oci.Process) (
+	userIDName IDName,
+	groupIDNames []IDName,
+	umask string,
+	err error,
+) {
 	rootPath := filepath.Join(guestpath.LCOWRootPrefixInUVM, containerID, guestpath.RootfsPath)
 	passwdPath := filepath.Join(rootPath, "/etc/passwd")
 	groupPath := filepath.Join(rootPath, "/etc/group")
 
 	if process == nil {
-		return IDName{}, nil, "", errors.New("spec.Process is nil")
+		err = errors.New("spec.Process is nil")
+		return
 	}
 
 	// this default value is used in the Linux kernel if no umask is specified
-	umask := "0022"
+	umask = "0022"
 	if process.User.Umask != nil {
 		umask = fmt.Sprintf("%04o", *process.User.Umask)
 	}
 
-	if process.User.Username != "" {
-		uid, gid, err := specGuest.ParseUserStr(rootPath, process.User.Username)
-		if err == nil {
-			userIDName := IDName{ID: strconv.FormatUint(uint64(uid), 10)}
-			groupIDName := IDName{ID: strconv.FormatUint(uint64(gid), 10)}
-			return userIDName, []IDName{groupIDName}, umask, nil
-		}
-		log.G(context.Background()).WithError(err).Warn("failed to parse user str, fallback to lookup")
-	}
-
-	// fallback UID/GID lookup
-	uid := process.User.UID
-	userIDName := IDName{ID: strconv.FormatUint(uint64(uid), 10), Name: ""}
-	if _, err := os.Stat(passwdPath); err == nil {
-		userInfo, err := getUser(passwdPath, func(user user.User) bool {
-			return uint32(user.Uid) == uid
-		})
-
-		if err != nil {
-			return userIDName, nil, "", err
-		}
-
-		userIDName.Name = userInfo.Name
-	}
-
-	gid := process.User.GID
-	groupIDName := IDName{ID: strconv.FormatUint(uint64(gid), 10), Name: ""}
-
+	parsedUserStr := false
 	checkGroup := true
-	if _, err := os.Stat(groupPath); err == nil {
-		groupInfo, err := getGroup(groupPath, func(group user.Group) bool {
-			return uint32(group.Gid) == gid
-		})
 
-		if err != nil {
-			return userIDName, nil, "", err
+	if process.User.Username != "" {
+		var usrInfo specGuest.ParseUserStrResult
+		usrInfo, err = specGuest.ParseUserStr(rootPath, process.User.Username)
+		if err == nil {
+			userIDName = IDName{ID: strconv.FormatUint(uint64(usrInfo.UID), 10), Name: usrInfo.Username}
+			groupIDNames = []IDName{
+				{ID: strconv.FormatUint(uint64(usrInfo.GID), 10), Name: usrInfo.Groupname},
+			}
+			parsedUserStr = true
+		} else {
+			log.G(context.Background()).WithError(err).Warnf("failed to parse user str %q, fallback to lookup", process.User.Username)
+			err = nil
 		}
-		groupIDName.Name = groupInfo.Name
-	} else {
-		checkGroup = false
 	}
 
-	groupIDNames := []IDName{groupIDName}
+	if !parsedUserStr {
+		// fallback UID/GID lookup
+		uid := process.User.UID
+		userIDName = IDName{ID: strconv.FormatUint(uint64(uid), 10), Name: ""}
+		if _, err = os.Stat(passwdPath); err == nil {
+			var userInfo user.User
+			userInfo, err = getUser(passwdPath, func(user user.User) bool {
+				return uint32(user.Uid) == uid
+			})
+
+			if err != nil {
+				return userIDName, nil, "", err
+			}
+
+			userIDName.Name = userInfo.Name
+		}
+
+		gid := process.User.GID
+		groupIDName := IDName{ID: strconv.FormatUint(uint64(gid), 10), Name: ""}
+
+		if _, err = os.Stat(groupPath); err == nil {
+			var groupInfo user.Group
+			groupInfo, err = getGroup(groupPath, func(group user.Group) bool {
+				return uint32(group.Gid) == gid
+			})
+
+			if err != nil {
+				return userIDName, nil, "", err
+			}
+			groupIDName.Name = groupInfo.Name
+		} else {
+			checkGroup = false
+			err = nil
+		}
+
+		groupIDNames = []IDName{groupIDName}
+	}
+
 	additionalGIDs := process.User.AdditionalGids
 	if len(additionalGIDs) > 0 {
 		for _, gid := range additionalGIDs {
-			groupIDName = IDName{ID: strconv.FormatUint(uint64(gid), 10), Name: ""}
+			groupIDName := IDName{ID: strconv.FormatUint(uint64(gid), 10), Name: ""}
 			if checkGroup {
-				groupInfo, err := getGroup(groupPath, func(group user.Group) bool {
+				var groupInfo user.Group
+				groupInfo, err = getGroup(groupPath, func(group user.Group) bool {
 					return uint32(group.Gid) == gid
 				})
 				if err != nil {

internal/guest/spec/spec.go
@@ -165,82 +165,99 @@ func SetCoreRLimit(spec *oci.Spec, value string) error {
 	return nil
 }
 
+type ParseUserStrResult struct {
+	UID       uint32
+	GID       uint32
+	Username  string
+	Groupname string
+}
+
 // ParseUserStr parses `userStr`, looks up container filesystem's /etc/passwd and /etc/group
 // files for UID and GID for the process.
 //
 // NB: When `userStr` represents a UID, which doesn't exist, return UID as is with GID set to 0.
-func ParseUserStr(rootPath, userStr string) (uint32, uint32, error) {
+func ParseUserStr(rootPath, userStr string) (res ParseUserStrResult, err error) {
 	parts := strings.Split(userStr, ":")
-	switch len(parts) {
-	case 1:
-		v, err := strconv.Atoi(parts[0])
-		if err != nil {
-			// this is a username string, evaluate uid/gid
-			u, err := getUser(rootPath, func(u user.User) bool {
-				return u.Name == userStr
-			})
-			if err != nil {
-				return 0, 0, errors.Wrapf(err, "failed to find user by username: %s", userStr)
-			}
-			return uint32(u.Uid), uint32(u.Gid), nil
-		}
+	if len(parts) > 2 {
+		return res, fmt.Errorf("invalid userstr: '%s'", userStr)
+	}
 
-		// this is a UID, parse /etc/passwd to find GID.
-		u, err := getUser(rootPath, func(u user.User) bool {
+	var v int
+	var u user.User
+	var g user.Group
+
+	// Handle the "user part" first.
+	v, err = strconv.Atoi(parts[0])
+	if err != nil {
+		// this is a username string, evaluate uid/gid
+		u, err = getUser(rootPath, func(u user.User) bool {
+			return u.Name == parts[0]
+		})
+		if err != nil {
+			return res, errors.Wrapf(err, "failed to find user by username: %s", parts[0])
+		}
+		res.UID = uint32(u.Uid)
+		res.GID = uint32(u.Gid)
+		res.Username = u.Name
+		// Will determine group name below
+	} else {
+		// this is a UID, parse /etc/passwd to find name and GID.
+		u, err = getUser(rootPath, func(u user.User) bool {
 			return u.Uid == v
 		})
 		if err != nil {
-			// UID doesn't exist, return as is with GID 0.
 			if OutOfUint32Bounds(v) {
-				return 0, 0, errors.Errorf("UID (%d) exceeds uint32 bounds", v)
+				return res, errors.Errorf("UID (%d) exceeds uint32 bounds", v)
 			}
-			return uint32(v), 0, nil
-		}
-		return uint32(u.Uid), uint32(u.Gid), nil
-	case 2:
-		var (
-			userName, groupName string
-			uid, gid            uint32
-		)
-		v, err := strconv.Atoi(parts[0])
-		if err != nil {
-			// username string, lookup UID
-			userName = parts[0]
-			u, err := getUser(rootPath, func(u user.User) bool {
-				return u.Name == userName
-			})
-			if err != nil {
-				return 0, 0, errors.Wrapf(err, "failed to find user by username: %s", userName)
-			}
-			uid = uint32(u.Uid)
+			// UID doesn't exist, continue with GID default to 0 (but we still want to
+			// look up its name) unless overridden.
+			res.UID = uint32(v)
 		} else {
-			if OutOfUint32Bounds(v) {
-				return 0, 0, errors.Errorf("UID (%d) exceeds uint32 bounds", v)
-			}
-			uid = uint32(v)
+			res.UID = uint32(u.Uid)
+			res.GID = uint32(u.Gid)
+			res.Username = u.Name
 		}
-
-		v, err = strconv.Atoi(parts[1])
-		if err != nil {
-			// groupname string, lookup GID
-			groupName = parts[1]
-			g, err := getGroup(rootPath, func(g user.Group) bool {
-				return g.Name == groupName
-			})
-			if err != nil {
-				return 0, 0, errors.Wrapf(err, "failed to find group by groupname: %s", groupName)
-			}
-			gid = uint32(g.Gid)
-		} else {
-			if OutOfUint32Bounds(v) {
-				return 0, 0, errors.Errorf("GID (%d) exceeds uint32 bounds", v)
-			}
-			gid = uint32(v)
-		}
-		return uid, gid, nil
-	default:
-		return 0, 0, fmt.Errorf("invalid userstr: '%s'", userStr)
 	}
+
+	if len(parts) == 1 {
+		g, err = getGroup(rootPath, func(g user.Group) bool {
+			return g.Gid == int(res.GID)
+		})
+		// If GID doesn't exist we just don't fill in groupName
+		if err == nil {
+			res.Groupname = g.Name
+		}
+		return res, nil
+	}
+
+	v, err = strconv.Atoi(parts[1])
+	if err != nil {
+		// this is a group name string, evaluate gid
+		g, err = getGroup(rootPath, func(g user.Group) bool {
+			return g.Name == parts[1]
+		})
+		if err != nil {
+			return res, errors.Wrapf(err, "failed to find group by groupname: %s", parts[1])
+		}
+		res.GID = uint32(g.Gid)
+		res.Groupname = g.Name
+	} else {
+		// this is a GID, parse /etc/group to find name.
+		g, err = getGroup(rootPath, func(g user.Group) bool {
+			return g.Gid == v
+		})
+		if err != nil {
+			if OutOfUint32Bounds(v) {
+				return res, errors.Errorf("GID (%d) exceeds uint32 bounds", v)
+			}
+			// GID doesn't exist, continue with GID as is.
+			res.GID = uint32(v)
+		} else {
+			res.GID = uint32(g.Gid)
+			res.Groupname = g.Name
+		}
+	}
+	return res, nil
 }
 
 // SetUserStr sets `spec.Process` to the valid `userstr` based on the OCI Image Spec
@@ -264,12 +281,12 @@ func ParseUserStr(rootPath, userStr string) (uint32, uint32, error) {
 func SetUserStr(spec *oci.Spec, userstr string) error {
 	setProcess(spec)
 
-	uid, gid, err := ParseUserStr(spec.Root.Path, userstr)
+	usrInfo, err := ParseUserStr(spec.Root.Path, userstr)
 	if err != nil {
 		return err
 	}
 
-	spec.Process.User.UID, spec.Process.User.GID = uid, gid
+	spec.Process.User.UID, spec.Process.User.GID = usrInfo.UID, usrInfo.GID
 	return nil
 }
 