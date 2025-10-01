7289723735319325d03a504911a3b51726625795
Tingmao Wang <tingmaowang@microsoft.com>
2025-10-01 14:15:27 +0100

    gcs, securitypolicyenforcer_rego: Add UnrecoverableError to replace panics in new code
    Per discussion with Ken, we concluded that the behaviour of panics is
    unpredictable / undesirable, and gcs should go into a hang state instead.
    
    The intention is to eventually replace all panic / log.Fatal with this.
    
    Suggested-by: Ken Gordon <kegordo@microsoft.com>
    Signed-off-by: Tingmao Wang <tingmaowang@microsoft.com>

--- pkg/securitypolicy/securitypolicyenforcer_rego.go
+++ pkg/securitypolicy/securitypolicyenforcer_rego.go
@@ -15,6 +15,7 @@ import (
 	"sync"
 	"syscall"
 
+	"github.com/Microsoft/hcsshim/internal/gcs"
 	"github.com/Microsoft/hcsshim/internal/guestpath"
 	"github.com/Microsoft/hcsshim/internal/log"
 	rpi "github.com/Microsoft/hcsshim/internal/regopolicyinterpreter"
@@ -1216,14 +1217,14 @@ func (policy *regoEnforcer) StartRevertableSection() (RevertableSectionHandle, e
 
 func (sh *revertableSectionHandle) Commit() {
 	if sh.policy == nil {
-		panic("revertable section handle already used")
+		gcs.UnrecoverableError(errors.New("revertable section handle already used"))
 	}
 
 	policy := sh.policy
 	sh.policy = nil
 	lockSucc := policy.revertableSectionLock.TryLock()
 	if lockSucc {
-		panic("not in a revertable section")
+		gcs.UnrecoverableError(errors.New("not in a revertable section"))
 	} else {
 		// somebody else (i.e. the caller) has the lock, so we're in a revertable
 		// section.  Clear the saved metadata just in case, then unlock to exit the
@@ -1235,20 +1236,20 @@ func (sh *revertableSectionHandle) Commit() {
 
 func (sh *revertableSectionHandle) Rollback() {
 	if sh.policy == nil {
-		panic("revertable section handle already used")
+		gcs.UnrecoverableError(errors.New("revertable section handle already used"))
 	}
 
 	policy := sh.policy
 	sh.policy = nil
 	lockSucc := policy.revertableSectionLock.TryLock()
 	if lockSucc {
-		panic("not in a revertable section")
+		gcs.UnrecoverableError(errors.New("not in a revertable section"))
 	} else {
 		// somebody else (i.e. the caller) has the lock, so we're in a revertable
 		// section.  Restore the saved metadata, then unlock to exit the section.
 		err := policy.rego.RestoreMetadata(policy.savedMetadata)
 		if err != nil {
-			panic(errors.Wrap(err, "unable to restore metadata for revertable section"))
+			gcs.UnrecoverableError(errors.Wrap(err, "unable to restore metadata for revertable section"))
 		}
 		policy.revertableSectionLock.Unlock()
 	}
